fc8a82fd3db1345cb2da3c7f024bf6e74204a3c1
farhan ansari <farhanraza2239@gmail.com>
2024-05-12 01:05:09 +0530

    a small change in api

--- controller/pg.controller.js
+++ controller/pg.controller.js
@@ -6,9 +6,7 @@ import uploadImagesToCloudinary from "../utility/cloudinary.upload.js";
 // next(errorHandler(403,'please fill all required field'))
 
 const addPgProperty = async (req, res, next) => {
-  // console.log("ye body hai ", req.body);
-  // console.log("yeh files hai ", req.files);
-  // console.log('ye controller me hai',req.cookies.token);
+ 
   const {
     roomSharing,
     balcony,
@@ -33,30 +31,68 @@ const addPgProperty = async (req, res, next) => {
     roomFacilities,
   } = req.body;
 
-  // const { images } = req.files;
+
 
   try {
-    if (
-      !roomSharing ||
-      !balcony ||
-      !availableFor ||
-      !rentAmount > 0 ||
-      !depositAmount ||
-      !city ||
-      !state ||
-      !localAddress ||
-      !laundary ||
-      !description ||
-      !doorClosingTime ||
-      !kitchen ||
-      !pgOrHostelName ||
-      !placeAvaibility ||
-      !roomCleaning ||
-      !warden ||
-      !foodAvaibility
-    ) {
-      return next(errorHandler(403, "please fill all required field"));
+    
+
+    if(!roomSharing){
+      // later on you can make a middleware an there you check all required filed
+      return next(errorHandler(403,'room sharing field is required'))
     }
+    if(!balcony){
+      return next(errorHandler(403,'balcony field is required'))
+    }
+    if(!availableFor){
+      return next(errorHandler(403,'available for field is required'))
+    }
+    if(!state){
+      return next(errorHandler(403,'state field is required'))
+    }
+    if(!city){
+      return next(errorHandler(403,'city field is required'))
+    }
+   
+    if(!localAddress){
+      return next(errorHandler(403,'local addresss field is required'))
+    }
+    if(!roomFacilities){
+      return next(errorHandler(403,'room facility field is required'))
+    }
+    if(!warden){
+      return next(errorHandler(403,'warden field is required'))
+    }
+    if(!description ){
+      return next(errorHandler(403,'dexcription field is required'))
+    }
+    if(!rentAmount>0){
+      return next(errorHandler(403,'rent field is required'))
+    }
+    if(!depositAmount>0){
+      return next(errorHandler(403,'deposit field is required'))
+    }
+    if(rentAmount<depositAmount){
+      return next(errorHandler(403,'deposit  can not be less than rent '))
+    }
+    if(!laundary){
+      return next(errorHandler(403,'laundary field is required'))
+    }
+    if(!doorClosingTime){
+      return next(errorHandler(403,'door closing field is required'))
+    }
+    if(!kitchen){
+      return next(errorHandler(403,'kitchen field is required'))
+    }
+    if(!pgOrHostelName){
+      return next(errorHandler(403,'pg/hostel name field is required'))
+    }
+    if(!roomCleaning){
+      return next(errorHandler(403,'room cleaning field is required'))
+    }
+    if(!foodAvaibility){
+      return next(errorHandler(403,'food avaibility field is required'))
+    }
+
     if (!req.files.length > 0) {
       return next(errorHandler(403, "please upload images"));
     }
@@ -98,9 +134,9 @@ const addPgProperty = async (req, res, next) => {
       images: imageUrls,
     });
 
-    const saveListing=await newListing.save()
+    const lsiting=await newListing.save()
 
-    res.json({success:true, msg: "create listing successfull" });
+    res.json({success:true, msg: "create listing successfull" ,lsiting});
   } catch (error) {
     console.log(error.message);
     next(errorHandler(500,'internal server error'))
