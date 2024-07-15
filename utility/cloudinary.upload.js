b112a1523714300b8ab83c5796ca5f4b9fee918e
farhan ansari <farhanraza2239@gmail.com>
2024-07-15 19:00:29 +0530

    fixing .env variable

--- utility/cloudinary.upload.js
+++ utility/cloudinary.upload.js
@@ -3,15 +3,21 @@ import { v2 as cloudinary } from 'cloudinary';
 
 // console.log(process.env.CLOUD_API_KEY,process.env.CLOUD_NAME,process.env.CLOUD_API_SECRET)
 
-// Cloudinary configuration
-cloudinary.config({
-  cloud_name: 'dhj7ys0nn',
-  api_key: '973758911928742',
-  api_secret: 'bC3reHCcAsKdfNVdcaVBZEdaagM'
-});
+
+
 
 // Function to upload files to Cloudinary
 const uploadImagesToCloudinary = async (files, userId) => {
+//   console.log('cloud api', process.env.CLOUD_API_KEY)
+// console.log('cloud name',process.env.CLOUD_NAME)
+// console.log('secret key',process.env.CLOUD_API_SECRET)
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+});
+
     try {
       const urls = [];
       for (const file of files) {
