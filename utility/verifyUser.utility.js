2555d7032b8e89fec081cdb47c83584137796521
farhan ansari <farhanraza2239@gmail.com>
2024-07-15 19:33:58 +0530

    test purpose

--- utility/verifyUser.utility.js
+++ utility/verifyUser.utility.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 const verifyUser = async (req, res, next) => {
   try {
     // Check token in cookies or headers
-    
+    console.log(req.headers)
     const token =
       req.cookies.access_token ||
       (req.headers.authorization
