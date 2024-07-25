462fba36cc47ffd368d06a2ff35962be07b27fef
farhan ansari <farhanraza2239@gmail.com>
2024-07-26 01:22:30 +0530

    a small changes in user model

--- models/user.model.js
+++ models/user.model.js
@@ -22,7 +22,7 @@ const userSchema=mongoose.Schema({
     phoneNumber:{
         type:Number,
         required:true,
-        unique:true,
+        // unique:true,
     },
     password:{
         type:String,
